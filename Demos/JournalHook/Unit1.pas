0264ec3c05e456202102bae35cd94bd451f05cce
fschetterer <fschetterer@outlook.com>
2019-10-13 18:57:03 -0700

    Update Unit1.pas

--- Demos/JournalHook/Unit1.pas
+++ Demos/JournalHook/Unit1.pas
@@ -37,7 +37,7 @@ implementation
 {$R *.dfm}
 
 {$IFDEF USEDLL}
-const HooksDLL = 'Hooks.DLL';
+const HooksDLL = ''JournalHooks.DLL';
 function HookActive: Boolean; external HooksDLL;
 function StartRecording: Boolean; external HooksDLL;
 function StartPlayback(const InstantPlayback: Boolean): Boolean; external HooksDLL;
